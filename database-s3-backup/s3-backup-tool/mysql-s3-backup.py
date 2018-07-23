8194f77a5f7846f2e6c5c50910457da66d13f0ec
Jorge Hevia <jmhev@outlook.com>
2018-07-23 08:05:45 +0200

    Commented the code

--- database-s3-backup/s3-backup-tool/mysql-s3-backup.py
+++ database-s3-backup/s3-backup-tool/mysql-s3-backup.py
@@ -2,6 +2,13 @@
 import subprocess
 import sys
 
+'''
+    Name: Backup
+    Function: this class contains the functions and
+        variables to make a dump from a given DB
+        and then upload it to a S3 bucket
+'''
+
 
 class Backup:
     database = ""
@@ -11,13 +18,16 @@ class Backup:
     node = ""
     file = ""
 
+    # inits the object with default values
     def __init__(self):
         self.database = ""
         self.bucket = ""
         self.user = ""
         self.password = ""
+        # gets the hostname to diferenciate the backups
         self.node = subprocess.getoutput("hostname")
 
+    # prints a help screen showing how to use the script
     def Help(self):
         print(
             """
@@ -37,6 +47,17 @@ class Backup:
 
         #######################
             """)
+    '''
+        Name: GetVariables
+        Input: nothing
+        Output: nothing
+        Function: given a user input while launching
+            the script, the function saves the input
+            into the class variables. If there're no
+            variables missing, the script goes on and
+            if at least one variable is missing, it will
+            show the help screen
+    '''
 
     def GetVariables(self):
         argv = sys.argv
@@ -50,10 +71,23 @@ class Backup:
                     self.user = argv[i + 1]
                 if '-p' == arg:
                     self.password = argv[i + 1]
+            # once it validates the input, continues to
+            # create the dump
             self.MySQLDump()
+            # once the dump is finished, it uploads it
+            # to S3
             self.UploadS3()
         else:
             self.Help()
+
+    '''
+        Name: MySQLDump
+        Input: nothing
+        Output: nothing
+        Function: given the class variables, executes the
+            mysqldump program with them and then compress
+            the dump with gzip
+    '''
 
     def MySQLDump(self):
         date = subprocess.getoutput("date +%Y%m%d-%H%M")
@@ -64,6 +98,14 @@ class Backup:
             " " + str(self.database) + " | gzip > " + self.file
         subprocess.call(call, shell=True, stdout=subprocess.DEVNULL)
 
+    '''
+        Name: UploadS3
+        Input: nothing
+        Output: nothing
+        Function: uploads the gziped dump into a S3 bucket
+            specified by the input
+    '''
+
     def UploadS3(self):
         check = "cat " + self.file
         while(subprocess.call(check, shell=True,
@@ -71,6 +113,8 @@ class Backup:
             print("please be patient...")
         call = "aws s3 cp " + self.file + " s3://" + self.bucket
         subprocess.call(call, shell=True, stdout=subprocess.DEVNULL)
+
+# gets the script running
 
 
 if __name__ == '__main__':
