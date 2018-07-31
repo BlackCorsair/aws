5619be1556ef81bb073c0fc87997924dbc1b5ef4
Jorge Hevia <jmhev@outlook.com>
2018-07-31 09:31:01 +0200

    Updated the cronjob

--- database-s3-backup/s3-backup-tool/cronjob.sh
+++ database-s3-backup/s3-backup-tool/cronjob.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/s3upload/venv/bin/activate
-/opt/s3upload/mysql-s3-backup.py
+/opt/s3upload/mysql-s3-backup.py # put here your options
+/opt/s3upload/postgres-s3-backup.py # put here your options
 rm -rf /tmp/*.sql.gz
